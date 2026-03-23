fd9c954ed4f1af1993fd433bc8cbe2dfe94f5a50
CHANGJY <neusoft.n.yang@mercedes-benz.com>
2026-03-23 13:34:18 +0800

    APRICOT-999466 fix:2 times ANR occur in com.mercedes.devicemanager when execute the monkey test for 12h
    Cherry-picked commit: c5a6580d2a5271224204290f2540f832495333cc
    (Only btcore content included).

---
+++ btcore/src/main/java/com/mercedes/btcore/broadcast/PhoneActivationBroadcastUtils.kt
@@ -0,0 +1,41 @@
+package com.mercedes.btcore.broadcast
+
+import android.content.Context
+import android.content.Intent
+import android.os.UserManager
+import com.mercedes.btcore.utils.logd
+
+object PhoneActivationBroadcastUtils {
+    fun sendActivationStartEvent(context: Context, userIdSafe: Int) {
+        sendEvent(context, userIdSafe, PhoneActivationStatus.ACTIVATION_START)
+    }
+
+    fun sendActivationEndEvent(context: Context, userIdSafe: Int) {
+        sendEvent(context, userIdSafe, PhoneActivationStatus.ACTIVATION_END)
+    }
+
+    fun sendDeactivationStartEvent(context: Context, userIdSafe: Int) {
+        sendEvent(context, userIdSafe, PhoneActivationStatus.DEACTIVATION_START)
+    }
+
+    fun sendDeactivationEndEvent(context: Context, userIdSafe: Int) {
+        sendEvent(context, userIdSafe, PhoneActivationStatus.DEACTIVATION_END)
+    }
+
+    private fun sendEvent(context: Context, userIdSafe: Int, status: PhoneActivationStatus) {
+        try {
+            val intent =
+                Intent(PhoneActivationStatusReceiver.ACTION_PHONE_ACTIVATION_STATUS_CHANGE).apply {
+                    setPackage(context.packageName)
+                    putExtra(PhoneActivationStatusReceiver.KEY_USER_ID, userIdSafe)
+                    putExtra(PhoneActivationStatusReceiver.KEY_STATUS, status.name)
+                }
+            val userManager = context.getSystemService(UserManager::class.java) as UserManager
+            userManager.userProfiles.forEach {
+                context.sendBroadcastAsUser(intent, it)
+            }
+        } catch (e: Exception) {
+            "sendEvent failed $e".logd()
+        }
+    }
+}
